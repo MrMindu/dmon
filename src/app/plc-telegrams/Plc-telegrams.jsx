b8cb315cd0868b0b9b8d52cc79133d594bd23ece
MrMindu <mindaugas.niunevas@gmail.com>
2023-10-23 15:50:58 +0300

    for sap cloud

--- src/app/plc-telegrams/Plc-telegrams.jsx
+++ src/app/plc-telegrams/Plc-telegrams.jsx
@@ -1,76 +1,77 @@
 "use client";
 
-import React, { useMemo, useState } from "react";
+import React from "react";
+// import React, { useMemo, useState } from "react";
 import data from "../data/Telegrams.json";
-import {
-  MaterialReactTable,
-  useMaterialReactTable,
-} from "material-react-table";
+// import {
+//   MaterialReactTable,
+//   useMaterialReactTable,
+// } from "material-react-table";
 
 function PLCTelegrams() {
   //data and fetching state
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isRefetching, setIsRefetching] = useState(false);
-  const [rowCount, setRowCount] = useState(0);
+  // const [isError, setIsError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isRefetching, setIsRefetching] = useState(false);
+  // const [rowCount, setRowCount] = useState(0);
 
-  //table state
-  const [columnFilters, setColumnFilters] = useState([]);
-  const [globalFilter, setGlobalFilter] = useState("");
-  const [sorting, setSorting] = useState([]);
-  const [pagination, setPagination] = useState({
-    pageIndex: 0,
-    pageSize: 10,
-  });
+  // //table state
+  // const [columnFilters, setColumnFilters] = useState([]);
+  // const [globalFilter, setGlobalFilter] = useState("");
+  // const [sorting, setSorting] = useState([]);
+  // const [pagination, setPagination] = useState({
+  //   pageIndex: 0,
+  //   pageSize: 10,
+  // });
 
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "PLC",
-        header: "PLC",
-      },
-      {
-        accessorKey: "TelegramSender",
-        header: "Sender",
-      },
-      {
-        accessorKey: "Receiver",
-        header: "Receiver",
-      },
-      {
-        accessorKey: "TUNo",
-        header: "TU Number",
-      },
-      {
-        accessorKey: "Telegram",
-        header: "Telegram",
-      },
-      //column definitions...
-    ],
-    []
-  );
+  // const columns = useMemo(
+  //   () => [
+  //     {
+  //       accessorKey: "PLC",
+  //       header: "PLC",
+  //     },
+  //     {
+  //       accessorKey: "TelegramSender",
+  //       header: "Sender",
+  //     },
+  //     {
+  //       accessorKey: "Receiver",
+  //       header: "Receiver",
+  //     },
+  //     {
+  //       accessorKey: "TUNo",
+  //       header: "TU Number",
+  //     },
+  //     {
+  //       accessorKey: "Telegram",
+  //       header: "Telegram",
+  //     },
+  //     //column definitions...
+  //   ],
+  //   []
+  // );
 
-  const table = useMaterialReactTable({
-    columns,
-    data,
-    enableRowActions: true,
-    initialState: {
-      //showColumnFilters: true,
-      rowsPerPage: -1,
-      pagination: { pageSize: 500 }
-    },
-    muiPaginationProps: {
-      color: "primary",
-      shape: "rounded",
-      showRowsPerPage: false,
-      variant: "outlined",
-    },
-    //paginationDisplayMode: "pages",
-  });
+  // const table = useMaterialReactTable({
+  //   columns,
+  //   data,
+  //   enableRowActions: true,
+  //   initialState: {
+  //     //showColumnFilters: true,
+  //     rowsPerPage: -1,
+  //     pagination: { pageSize: 500 }
+  //   },
+  //   muiPaginationProps: {
+  //     color: "primary",
+  //     shape: "rounded",
+  //     showRowsPerPage: false,
+  //     variant: "outlined",
+  //   },
+  //paginationDisplayMode: "pages",
+  // });
 
   return (
     <div className="telegrams">
-      <MaterialReactTable table={table} />
+      {/* <MaterialReactTable table={table} /> */}
       {/* <MaterialReactTable
         columns={columns}
         data={data}
@@ -82,7 +83,7 @@ function PLCTelegrams() {
           showLastButton: false,
         }}
       /> */}
-      {/* <table className="telegrams-table">
+      <table className="telegrams-table">
         <thead className="telegrams__thead">
           <tr>
             <th>PLC</th>
@@ -103,7 +104,7 @@ function PLCTelegrams() {
             </tr>
           ))}
         </tbody>
-      </table> */}
+      </table>
     </div>
   );
 }
