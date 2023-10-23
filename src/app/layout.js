5dd5d22d1491fd1cbb22b1940448654ec63cd2f7
MrMindu <mindaugas.niunevas@gmail.com>
2023-10-23 14:11:01 +0300

    build ready, kinda

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,4 @@
 "use client"
-import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import "../../public/global.css";
 import { Inter } from "next/font/google";
@@ -14,9 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <BrowserRouter>
           <Navbar />
-        </BrowserRouter>
         {children}
       </body>
     </html>
